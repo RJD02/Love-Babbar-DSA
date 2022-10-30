f7fd5e08bd8fe023bb9199c2265a422cafa16abb
Anish Rajwani <75964487+19ANISH@users.noreply.github.com>
2022-10-30 10:35:38 +0530

    Delete ReverseArray.py

--- Arrays/Reverse-the-array/ReverseArray.py
+++
@@ -1,8 +0,0 @@
-n = int(input())
-while(n!=0):
-    n-=1
-    t = int(input())
-    li = list(map(int,input().split()))[:t]
-    a = li[::-1]
-    b = ''.join(str(a).split(','))
-    print(b.rstrip(']').lstrip('['))
