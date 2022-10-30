47d580ca7cccb74c4cb0bd621ede53406986f0a7
RJD02 <dulangeraviraj@gmail.com>
2022-10-30 13:57:09 +0530

    Add solution

---
+++ Arrays/Kth max element/solution.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int solve(vector<int> v, int k) {
+	priority_queue<int> pq(v.begin(), v.end());
+	while(--k)
+		pq.pop();
+	return pq.top();
+}
+
+int main() {
+	int t;
+	cin >> t;
+	while(t--) {
+		int n, k;
+		cin >> n >> k;
+		vector<int> v(n);
+		for(int i = 0; i < n; i++)
+			cin >> v[i];
+		cout << solve(v, k);
+	}
+}
