71e69e2d9da30377b3e196b693f8f042c8f02b8e
Anish Rajwani <75964487+19ANISH@users.noreply.github.com>
2022-11-02 09:49:23 +0530

    Update solution.py

--- Arrays/Min-Max-Element/python/solution.py
+++ Arrays/Min-Max-Element/python/solution.py
@@ -1,1 +1,14 @@
+def getMinMax(a,n):
+	maxi = max(a)
+	mini = min(a)
+	return mini,maxi
 
+if __name__ == "__main__":
+	t = int(input())
+	while(t>0):
+		n = int(input())
+		a = [int (x) for x in input().strip().split()]
+		product = getMinMax(a,n)
+		print(product[0],end = " ")
+		print(product[1])
+		t-=1
